1e01edbc337a1d11203e0c452e293030dbc339cc
Shaun Thompson <neko68k@gmail.com>
2013-05-16 16:28:55 -0600

    minor fix to swap_word

--- Wipeout 4Ever/endian_tools.h
+++ Wipeout 4Ever/endian_tools.h
@@ -4,7 +4,7 @@
 #include <Windows.h>
 #include <stdio.h>
 
-#define SWAP_WORD(x) (x=(x>>8)|(x<<8))
+#define SWAP_WORD(x) (x=(x>>8)|((x<<8)&0x00FF))
 #define SWAP_DWORD(x) (x= (x>>24) | \
         ((x<<8) & 0x00FF0000) |\
         ((x>>8) & 0x0000FF00) |\
